e07af8bc2e23b78dc554fec5f9aeffaefae0e7a7
Evan Cameron <cameron.evan@gmail.com>
2017-09-15 23:20:09 -0400

    move impl for arena

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ impl<T: Ord> Tree<T> {
 }
 
 fn count<T: Ord>(tree: &Option<Box<Tree<T>>>) -> i32 {
+    // without box syntax
     // if let &Some(ref node) = tree {
     //     let node = &*node;
     //     1 + count(&node.l) + count(&node.r)
@@ -51,50 +52,3 @@ fn insert<T: Ord>(tree: &mut Option<Box<Tree<T>>>, item: T) {
 }
 
 fn main() {}
-
-
-// #[derive(PartialOrd, PartialEq)]
-// struct Tree<'a, T: 'a + Ord> {
-//     l: Option<&'a mut Tree<'a, T>>,
-//     r: Option<&'a mut Tree<'a, T>>,
-//     data: T,
-// }
-
-// impl<'a, T: 'a + Ord> Tree<'a, T> {
-//     fn new(t: T) -> Tree<'a, T> {
-//         Tree {
-//             l: None,
-//             data: t,
-//             r: None,
-//         }
-//     }
-// }
-
-
-// fn count<T: Ord>(tree: &Option<&mut Tree<T>>) -> i32 {
-//     if let &Some(&mut Tree { ref l, ref r, .. }) = tree {
-//         1 + count(l) + count(r)
-//     } else {
-//         0
-//     }
-// }
-// fn insert<'a, T: 'a + Ord>(tree: &Option<&'a mut Tree<'a, T>>, item: T) {
-//     if let &Some(&mut Tree { mut l, data, mut r }) = tree {
-//         if item < data {
-//             if l == None {
-//                 let t_: Tree<'a, T> = Tree::new(item);
-//                 l = Some(&mut t_);
-//             } else {
-//                 insert(&l, item);
-//             }
-//         } else if item > data {
-//             if r == None {
-//                 let t_: Tree<'a, T> = Tree::new(item);
-//                 r = Some(&mut t_);
-//             } else {
-//                 insert(&r, item);
-//             }
-//         }
-//     }
-// }
-// fn main() {}
---
+++ src/refs.rs
@@ -0,0 +1,45 @@
+// #[derive(PartialOrd, PartialEq)]
+// struct Tree<'a, T: 'a + Ord> {
+//     l: Option<&'a mut Tree<'a, T>>,
+//     r: Option<&'a mut Tree<'a, T>>,
+//     data: T,
+// }
+
+// impl<'a, T: 'a + Ord> Tree<'a, T> {
+//     fn new(t: T) -> Tree<'a, T> {
+//         Tree {
+//             l: None,
+//             data: t,
+//             r: None,
+//         }
+//     }
+// }
+
+
+// fn count<T: Ord>(tree: &Option<&mut Tree<T>>) -> i32 {
+//     if let &Some(&mut Tree { ref l, ref r, .. }) = tree {
+//         1 + count(l) + count(r)
+//     } else {
+//         0
+//     }
+// }
+// fn insert<'a, T: 'a + Ord>(tree: &Option<&'a mut Tree<'a, T>>, item: T) {
+//     if let &Some(&mut Tree { mut l, data, mut r }) = tree {
+//         if item < data {
+//             if l == None {
+//                 let t_: Tree<'a, T> = Tree::new(item);
+//                 l = Some(&mut t_);
+//             } else {
+//                 insert(&l, item);
+//             }
+//         } else if item > data {
+//             if r == None {
+//                 let t_: Tree<'a, T> = Tree::new(item);
+//                 r = Some(&mut t_);
+//             } else {
+//                 insert(&r, item);
+//             }
+//         }
+//     }
+// }
+// fn main() {}
