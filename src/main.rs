3bdc3d290351f94fdd01bcb9874e894865d5a74c
Evan Cameron <evan@vectorface.com>
2017-09-18 16:52:03 -0400

    wrote fold for tree, add type alias
    I wanted to write a recursive preorder type traversal,
    I started with a closure, it turns out you can't recursively
    call a closure, and there's some unsafety there. So I went
    with iterative traversal using a stack.
    
    In reality, I should impl IntoIterator and Iterator and have
    all these fns for free.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,32 @@
 #![feature(box_syntax, box_patterns)]
 
-#[derive(PartialOrd, PartialEq)]
+type MyTree<T> = Option<Box<Tree<T>>>;
+
+#[derive(PartialOrd, PartialEq, Debug)]
 struct Tree<T: Ord> {
-    l: Option<Box<Tree<T>>>,
-    r: Option<Box<Tree<T>>>,
+    l: MyTree<T>,
+    r: MyTree<T>,
     data: T,
 }
 
 impl<T: Ord> Tree<T> {
-    fn new(t: T) -> Tree<T> {
+    fn new(data: T) -> Tree<T> {
         Tree {
             l: None,
-            data: t,
+            data,
             r: None,
         }
     }
+    fn new_box(data: T) -> MyTree<T> {
+        Some(box Tree {
+            l: None,
+            data,
+            r: None,
+        })
+    }
 }
 
-fn count<T: Ord>(tree: &Option<Box<Tree<T>>>) -> i32 {
+fn len<T: Ord>(tree: &MyTree<T>) -> i32 {
     // without box syntax
     // if let &Some(ref node) = tree {
     //     let node = &*node;
@@ -26,12 +35,13 @@ fn count<T: Ord>(tree: &Option<Box<Tree<T>>>) -> i32 {
     //     0
     // }
     if let &Some(box Tree { ref l, ref r, .. }) = tree {
-        1 + count(l) + count(r)
+        1 + len(l) + len(r)
     } else {
-        1
+        0
     }
 }
-fn insert<T: Ord>(tree: &mut Option<Box<Tree<T>>>, item: T) {
+
+fn insert<T: Ord>(tree: &mut MyTree<T>, item: T) {
     if let &mut Some(ref mut node) = tree {
         if item < node.data {
             if node.l == None {
@@ -51,4 +61,45 @@ fn insert<T: Ord>(tree: &mut Option<Box<Tree<T>>>, item: T) {
     }
 }
 
-fn main() {}
+fn fold<T: Ord, B, F>(tree: &MyTree<T>, init: B, mut f: F) -> B
+where
+    F: for <'a> FnMut(B, &'a T) -> B,
+{
+    let mut acc = init;
+    if let &Some(ref node) = tree {
+        let node = &*node;
+        let mut stack = vec![node];
+
+        while let Some(ref node) = stack.pop() { 
+            acc = f(acc, &node.data);
+            if let Some(ref right) = node.r {
+                stack.push(right);
+            }
+            if let Some(ref left) = node.l {
+                stack.push(left);
+            }
+        }
+
+        acc
+    } else {
+        acc
+    }
+}
+
+fn main() {
+    let mut tree = Tree::new_box(1);
+    insert(&mut tree, 2);
+    insert(&mut tree, 3);
+    insert(&mut tree, 4);
+    insert(&mut tree, 5);
+    insert(&mut tree, 6);
+    insert(&mut tree, 7);
+    insert(&mut tree, 8);
+    insert(&mut tree, -18);
+    insert(&mut tree, -10);
+    insert(&mut tree, -1);
+    insert(&mut tree, -2);
+
+    println!("{:?}", tree);
+    println!("{:?}", len(&tree));
+}
